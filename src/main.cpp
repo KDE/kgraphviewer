ce474327b4dfaa977a579061e862151865c5771e
Gaël de Chalendar <kleag@free.fr>
2007-12-28 13:35:05 +0000

    Updated the apps version numbers

--- src/main.cpp
+++ src/main.cpp
@@ -37,7 +37,7 @@
 static const char description[] =
 I18N_NOOP("A Graphviz dot graph viewer for KDE");
 
-static const char version[] = "1.0";
+static const char version[] = "1.99.1";
 
 int main(int argc, char **argv)
 {
