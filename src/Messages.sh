844fe7066f334d23d036ed724f2b0b19b9df3e8e
Laurent Montel <montel@kde.org>
2009-03-25 08:13:20 +0000

    Extract from kcfg

--- src/Messages.sh
+++ src/Messages.sh
@@ -1,3 +1,4 @@
 #! /usr/bin/env bash
-$EXTRACTRC `find . -name \*.ui -o -name \*.rc` >> rc.cpp
+$EXTRACTRC `find . -name \*.ui -o -name \*.rc -o -name \*.kcfg` >> rc.cpp
 $XGETTEXT *.cpp part/*.cpp -o $podir/kgraphviewer.pot
+rm -f rc.cpp
