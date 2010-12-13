a6aea731215abb9320dce0c6ecde6efafaf9e97d
Kevin Funk <krf@electrostorm.net>
2010-12-13 11:56:19 +0100

    Fix tests

--- tests/kgraphviz/dotgraphtests.cpp
+++ tests/kgraphviz/dotgraphtests.cpp
@@ -1,3 +1,22 @@
+/*
+    Copyright (C) 2010 Kevin Funk <krf@electrostorm.net>
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+
+    You should have received a copy of the GNU Library General Public License
+    along with this library; see the file COPYING.LIB.  If not, write to
+    the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+    Boston, MA 02110-1301, USA.
+*/
+
 #include <kgraphviz/dotgraph.h>
 
 #include <QtTest/QtTest>
@@ -10,7 +29,7 @@ private Q_SLOTS:
   void testCreateGraph();
 };
 
-using namespace KGraphViewer;
+using namespace KGraphViz;
 
 void DotGraphTests::testCreateGraph()
 {
@@ -22,4 +41,4 @@ void DotGraphTests::testCreateGraph()
 }
 
 QTEST_MAIN(DotGraphTests)
-#include "dotgraphtests.moc"*/
+#include "dotgraphtests.moc"
