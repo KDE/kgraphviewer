168646b1d076d44355b9c54dd94badaf963144f0
Reimar Döffinger <Reimar.Doeffinger@gmx.de>
2014-04-20 12:53:15 +0200

    Bump version number for the many recent changes.
    Signed-off-by: Reimar Döffinger <Reimar.Doeffinger@gmx.de>

--- src/main.cpp
+++ src/main.cpp
@@ -37,7 +37,7 @@
 static const char description[] =
 I18N_NOOP("A Graphviz dot graph viewer for KDE");
 
-static const char version[] = "2.1.1";
+static const char version[] = "2.2.0";
 
 int main(int argc, char **argv)
 {
