b2f7946e429db41725629644ff87b37276e85f4f
Friedrich W. H. Kossebau <kossebau@kde.org>
2017-03-27 00:36:33 +0200

    setupGUI() without Create does not set XML file, so do again explicitly

--- src/kgraphviewer.cpp
+++ src/kgraphviewer.cpp
@@ -61,6 +61,9 @@ KGraphViewerWindow::KGraphViewerWindow()
     : KParts::MainWindow(),
       m_rfa(0)
 {
+  // set the shell's ui resource file
+  setXMLFile("kgraphviewerui.rc");
+
 //   std::cerr << "Creating tab widget" << std::endl;
   m_widget = new QTabWidget(this);
   m_widget->setTabsClosable(true);
@@ -91,7 +94,7 @@ KGraphViewerWindow::KGraphViewerWindow()
   connect( m_manager, SIGNAL(activePartChanged(KParts::Part*)),
            this, SLOT(createGUI(KParts::Part*)) );
 
-  setupGUI(ToolBar | Keys | StatusBar | Save, QStringLiteral("kgraphviewerui.rc"));
+  setupGUI(ToolBar | Keys | StatusBar | Save);
 
   // then, setup our actions
   setupActions();
