9961c92736abfc6d0aedea9c4e7c679e1fd564d2
Nicolas Fella <nicolas.fella@gmx.de>
2022-03-07 19:49:21 +0100

    Enable highdpi pixmaps

--- src/main.cpp
+++ src/main.cpp
@@ -38,6 +38,7 @@
 
 int main(int argc, char **argv)
 {
+    QApplication::setAttribute(Qt::AA_UseHighDpiPixmaps);
     QApplication app(argc, argv);
 
     KLocalizedString::setApplicationDomain("kgraphviewer");
